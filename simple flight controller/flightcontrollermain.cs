38cbc2989b1b70bdb3234b9b19589fc5926e33c0
gouthamsrikar <53181084+gouthamsrikar@users.noreply.github.com>
2020-09-29 20:37:06 +0530

    Create  flightcontrollermain.cs

---
+++ simple flight controller/flightcontrollermain.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flightcontroller : MonoBehaviour
+{
+    public moveup jumpb;
+    public moveup jumpa;
+    public touchfield tf;
+    public float boostSpeed = 20f;
+    public FixedJoystick fj;
+
+    public float moveSpeed = 10f;
+    public float liftSpeed = 10f;
+    public float turnSpeed = 10f;
+   
+    public float bankSpeed = 10f;
+
+
+    void RotateUpDown(float axis)
+    {
+        transform.RotateAround(transform.position, transform.right, -axis * Time.deltaTime);
+    }
+    void RotateRightLeft(float axis)
+    {
+        transform.RotateAround(transform.position, Vector3.up, -axis * Time.deltaTime);
+    }
+    void Update()
+    {
+        if(jumpb.Pressed)
+        {
+            transform.Translate(Vector3.forward * boostSpeed * Time.deltaTime);
+        }
+
+        if (jumpa.Pressed)
+        {
+            transform.Translate(-Vector3.forward * boostSpeed * Time.deltaTime);
+        }
+
+        float xdis = tf.TouchDist.x;
+        float ydis = tf.TouchDist.y;
+        RotateRightLeft(-xdis);
+        RotateUpDown(ydis);
+        float x = fj.Horizontal;
+        float y = fj.Vertical;
+        if (y >0)
+        { transform.Translate(-Vector3.up * moveSpeed * Time.deltaTime,Space.Self );
+            y = 0;
+        }
+        else if(y<0)
+        { transform.Translate(Vector3.up * moveSpeed * Time.deltaTime, Space.Self);
+            y = 0;
+        }
+
+        if (x > 0)
+        {
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime, Space.Self);
+            y = 0;
+        }
+        else if (y < 0)
+        {
+            transform.Translate(Vector3.left * moveSpeed * Time.deltaTime, Space.Self);
+            y = 0;
+        }
+
+    }
+}
+    
