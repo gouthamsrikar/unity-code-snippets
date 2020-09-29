fc00f9446e378a244a3d9c3fef85070e9c1f354f
gouthamsrikar <53181084+gouthamsrikar@users.noreply.github.com>
2020-09-29 20:38:17 +0530

    Update flightcontrollermain.cs

--- simple flight controller/flightcontrollermain.cs
+++ simple flight controller/flightcontrollermain.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class flightcontroller : MonoBehaviour
+public class flightcontrollermain : MonoBehaviour
 {
     public moveup jumpb;
     public moveup jumpa;
