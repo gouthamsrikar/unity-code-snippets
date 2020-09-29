66e3791f1f645d321902fa8ba2210d2d923e26b6
gouthamsrikar <53181084+gouthamsrikar@users.noreply.github.com>
2020-09-29 20:40:07 +0530

    Add files via upload

---
+++ simple flight controller/jump.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System.Collections.Generic;
+
+public class jump : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
+{
+    [HideInInspector]
+    public bool Pressed;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Pressed = true;
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        Pressed = false;
+    }
+}
